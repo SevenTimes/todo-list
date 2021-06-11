35012a8e8337a96002a63c90befd7fa3f2385e76
SevenTimes <krab47@yahoo.com>
2021-06-11 17:02:06 +0400

    add delete and complete tasks functionality

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -6,6 +6,7 @@ class Task {
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.isCompleted = false;
   }
 }
 
@@ -13,7 +14,7 @@ function updateTaskList() {
   const taskList = document.getElementById('task-list');
   taskList.innerHTML = '';
   if (currentProject.tasks.length > 0) {
-    currentProject.tasks.forEach((task) => {
+    currentProject.tasks.forEach((task, index) => {
       const newTask = document.createElement('div');
       newTask.classList.add('task', `${task.priority}`);
 
@@ -28,6 +29,35 @@ function updateTaskList() {
       const date = document.createElement('span');
       date.innerText = task.dueDate;
       div.appendChild(date);
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'DEL';
+      deleteBtn.addEventListener('click', () => {
+        currentProject.tasks.splice(index, 1);
+        updateTaskList();
+      });
+      div.appendChild(deleteBtn);
+
+      const completeCheckbox = document.createElement('input');
+      completeCheckbox.setAttribute('type', 'checkbox');
+      if (task.isCompleted) {
+        completeCheckbox.checked = true;
+      }
+      completeCheckbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
+          task.isCompleted = true;
+        } else {
+          task.isCompleted = false;
+        }
+        updateTaskList();
+      });
+      div.appendChild(completeCheckbox);
+
+      if (task.isCompleted) {
+        newTask.className += ' completed';
+      } else {
+        newTask.classList.remove('completed');
+      }
 
       newTask.appendChild(div);
       taskList.appendChild(newTask);
